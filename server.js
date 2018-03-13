e0f45c6a186217c4644c5632bd8c8cc725a98fc8
Rares <mindricanmihai@gmail.com>
2018-03-13 17:21:45 +0200

    - added number of players

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io')({
-      "transports": ["xhr-polling"],
-      "polling duration":10
-    }).listen(server);
-
-let scoreResults=[];
-
-
-
-io.on('connection', (client) => {
-  client.on('subscribeToResults', (interval) => {
-    console.log('client is subscribing to results with interval ', interval);
+var express = require("express"),
+  app = express(),
+  server = require("http").createServer(app),
+  io = require("socket.io")({
+    transports: ["xhr-polling"],
+    "polling duration": 10
+  }).listen(server);
+let scoreResults = [];
+io.on("connection", client => {
+  client.on("subscribeToResults", interval => {
+    console.log("client is subscribing to results with interval ", interval);
     setInterval(() => {
-      client.emit('results',scoreResults );
+      client.emit("results", scoreResults);
     }, interval);
   });
 
-  client.on('chat message', (msg)=>{
+  client.on("showNumberOfPlayers", interval => {
+    var srvSockets = io.sockets.sockets;
+    
+    setInterval(() => {
+      client.emit("numberOfPlayer", Object.keys(srvSockets).length);
+    }, interval);
+  });
+
+  client.on("chat message", msg => {
     console.log(msg);
     scoreResults.push(msg);
   });
-  client.on('error',function () {
-  
-  });
+  client.on("error", function() {});
+
 });
 
 server.listen(process.env.PORT || 8021);
